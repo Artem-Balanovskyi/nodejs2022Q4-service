51b493bcc34adde79fc73897419c5030657f36f6
Artem Balanovskyi <mr.balanovsky@gmail.com>
2023-02-06 23:17:00 +0200

    feat: add swagger and edit api.yaml

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as yaml from 'js-yaml';
+import { readFile } from 'fs/promises';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -8,6 +11,10 @@ const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const document: any = yaml.load(
+    await readFile('./doc/api.yaml', { encoding: 'utf-8' }),
+  );
+  SwaggerModule.setup('doc', app, document);
   await app.listen(PORT);
 }
 bootstrap();
